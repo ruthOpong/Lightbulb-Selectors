7148604b9b075c906c9e979d96de6c527050a0eb
= <=>
2023-10-19 14:35:40 +0100

    done

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Write your code here
+let l1 = document.querySelector('#lightbulb1');
+let l2 = document.querySelector('#lightbulb2');
+let l3 = document.querySelector('#lightbulb3');
+let subtitle = document.querySelector('.subtitle');
+let bulb;
+let counter = 0
+
+function changeState (bulb){
+    console.log('workin');
+    const list = bulb.classList;
+    console.log(list);
+    list.toggle("active");
+    counter = counter + 1;
+    subtitle.innerHTML = "You've clicked the lights " + counter + " times";
+}
+
+l1.addEventListener('click', function(){changeState(l1)});
+l2.addEventListener('click', function(){changeState(l2)});
+l3.addEventListener('click', function(){changeState(l3)});
